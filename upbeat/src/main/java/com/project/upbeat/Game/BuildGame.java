3c0c5cbfd2ef1b0f524fe04a7dcae8e45166e5b2
jirayu <jirayu004za@gmail.com>
2023-04-02 18:52:45 +0700

    add clas BuildGame and detail of class, but not complete need implement

---
+++ upbeat/src/main/java/com/project/upbeat/Game/BuildGame.java
@@ -0,0 +1,16 @@
+package com.project.upbeat.Game;
+
+import com.project.upbeat.GameProcess.Game;
+import java.util.*;
+
+public class BuildGame{
+    private static Game buildGame;
+    private static final Map<String, Game> bindings = new HashMap<>();
+
+    // Need implement
+    public static Game getBuildGame(){
+        // Need implement
+
+        return null;
+    }
+}
