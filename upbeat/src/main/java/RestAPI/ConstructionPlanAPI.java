421572251327ab3c8d4650576621c39cac974680
jirayu <jirayu004za@gmail.com>
2023-04-02 19:54:55 +0700

    Add Empty class, not complete need to implement class to complete

---
+++ upbeat/src/main/java/RestAPI/ConstructionPlanAPI.java
@@ -0,0 +1,9 @@
+package RestAPI;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/RESTAPI")
+public class ConstructionPlanAPI{
+}
