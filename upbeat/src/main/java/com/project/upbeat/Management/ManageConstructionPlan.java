2342f1e9e3fa34a5a724e36fc2a2bbf658f8256a
jirayu <jirayu004za@gmail.com>
2023-04-02 21:48:26 +0700

    Add class ManageConstructionPlan and implement class to complete

--- upbeat/src/main/java/com/project/upbeat/Management/ManageConstructionPlan.java
+++ upbeat/src/main/java/com/project/upbeat/Management/ManageConstructionPlan.java
@@ -1,6 +1,7 @@
 package com.project.upbeat.Management;
 
 import com.project.upbeat.GameProcess.Game;
+import com.project.upbeat.GameProcess.GameUnit;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,34 @@ import org.springframework.stereotype.Service;
 public class ManageConstructionPlan{
     @Setter
     private static Game game;
+
+    public static Game SendConstructionPlan(String constructionPlan, int idOfPlayer) throws RuntimeException{
+        try{
+            if(game.getCurrentPlayer().getID() == idOfPlayer){
+                String planGame = constructionPlan;
+                game.getConstructionPlan(planGame);
+
+                return game;
+            }else{
+                throw new RuntimeException("Check you turn!!");
+            }
+        }catch(RuntimeException exception){
+            throw new RuntimeException(exception.getMessage());
+        }
+    }
+
+    private static Game mock = GameUnit.createGame("player1", "player2");
+    public  static  Game SendConstructionPlanMock(String constructionPlan, int idOfPlayer) throws RuntimeException{
+        try{
+            if(mock.getCurrentPlayer().getID() == idOfPlayer){
+                String planGame = constructionPlan;
+                mock.getConstructionPlan(planGame);
+                return mock;
+            }else{
+                throw new RuntimeException("Check you turn!!");
+            }
+        }catch(RuntimeException exception){
+            throw new RuntimeException(exception.getMessage());
+        }
+    }
 }
