a43e060de1645bff94aba7213f6c91dcc170fe66
jirayu <jirayu004za@gmail.com>
2023-04-02 19:52:47 +0700

    add variable in constructor

--- upbeat/src/main/java/com/project/upbeat/Game/BuildGame.java
+++ upbeat/src/main/java/com/project/upbeat/Game/BuildGame.java
@@ -1,16 +1,17 @@
 package com.project.upbeat.Game;
 
 import com.project.upbeat.GameProcess.Game;
+import com.project.upbeat.Request.ConfigurationRequest;
+
 import java.util.*;
 
 public class BuildGame{
     private static Game buildGame;
     private static final Map<String, Game> bindings = new HashMap<>();
 
-    // Need implement
-    public static Game getBuildGame(){
-        // Need implement
 
+    public static Game getBuildGame(String nameOfPlayer1, String nameOfPlayer2, ConfigurationRequest config){
+        // need implement constructor
         return null;
     }
 }
