da59ed27ef763b7a919078679f2c60b57f04748c
jirayu <jirayu004za@gmail.com>
2023-04-02 19:52:09 +0700

    add class and variable, not complete need implement

---
+++ upbeat/src/main/java/com/project/upbeat/Management/ManageConstructionPlan.java
@@ -0,0 +1,11 @@
+package com.project.upbeat.Management;
+
+import com.project.upbeat.GameProcess.Game;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ManageConstructionPlan{
+    @Setter
+    private static Game game;
+}
